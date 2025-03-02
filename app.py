c4122b033929a3dbc2c2b52080b3706248003c6a
rishik <rishikrc3@gmail.com>
2025-03-02 15:18:21 +0000

    create-get-routes

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+from repository import Repository  
+
+app = Flask(__name__)
+repo = Repository("tracks")  
+
+@app.route("/tracks", methods=["POST"])
+def add_track():
+    data = request.get_json()
+    title = data.get("title")
+    artist = data.get("artist")
+
+    if not title or not artist:
+        return jsonify({"error": "Missing title or artist"}), 400
+
+    track_id = repo.insert(title, artist)
+    
+    return jsonify({"message": "Track added", "track_id": track_id}), 201
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=5001, debug=True)
---
+++ repository.py
@@ -0,0 +1,25 @@
+import sqlite3
+class Repository:
+    def __init__(self, table):
+        self.table = table
+        self.database = self.table + ".db"
+        self.make()
+    
+    def make(self):
+        with sqlite3.connect(self.database) as connection:
+            cursor = connection.cursor()
+            cursor.execute("CREATE TABLE IF NOT EXISTS tracks (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, artist TEXT NOT NULL);")
+            connection.commit()
+    
+    def insert(self, title, artist):
+        with sqlite3.connect(self.database) as connection:
+            cursor = connection.cursor()
+            cursor.execute("INSERT INTO tracks (title, artist) VALUES (?, ?)", (title, artist))
+            connection.commit()
+            return cursor.lastrowid
+        
+    def get(self):
+        with sqlite3.connect(self.database) as connection:
+            cursor = connection.cursor()
+            cursor.execute("SELECT id, title, artist FROM tracks")
+            return cursor.fetchall()
